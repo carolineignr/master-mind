5733b307a59e3cfe95aba2bd9871bcb56831832f
Caroline Ignácio <carolineignacior@gmail.com>
2021-03-29 22:07:09 -0300

    Update master mind game

--- masterMind.js
+++ masterMind.js
@@ -2,17 +2,17 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 let guessesCounter = 0;
-let correctGuesses = 0;
 let correctPositions = 0;
+let correctGuesses = [];
 
 let foundPattern = false;
 
 let maxGuesses;
-let patternLength;
+let patternLength = 0;
+let patternGuess = [];
+const patternArr = [];
 
 function generatePattern() {
-    const patternArr = [];
-
     for(let i = 0; i < patternLength; i++) {
         let randomNumber = generateNumber();
 
@@ -25,44 +25,100 @@ function generatePattern() {
 }
 
 function valdiatePatternGuess(patternGuess) {
-    console.log('validando...')
-    return true;
+    for(let i = 0; i < patternLength; i++) { 
+        for (let j = 0; j < patternLength; j++) {
+            if (patternArr[i] === patternGuess[j]) {
+                if (!correctGuesses.includes(patternGuess[j])) correctGuesses.push(patternGuess[j]);
+
+                if (i === j) {
+                    correctPositions++;
+                }
+            }
+        }
+    }
+    return correctPositions === patternLength ? true : false;
+}
+
+function validateInput(value, func) {
+    if (isNaN(value)) {
+        console.log('String is not permitted. Please type an integer value.')
+        func();
+    }
+
+    if (value <= 0) {
+        console.log('Please type a value bigger than 0.')
+        func();
+    }
+
+    if (value > 6) {
+        console.log('Please type a value smaller or equal to 6.')
+        func();
+    }
 }
 
 function generateNumber() {
-    return Math.ceil(Math.random() * (6 - 1) + 1);
+    return Math.floor(Math.random() * (7 - 1) + 1);
+}
+
+function getPatternGuess() {
+    patternGuess = prompt(`Write your pattern guess with length size \
+        equal to ${patternLength}, please: `);;
+
+    patternGuess = patternGuess.split('');
+    patternGuess = patternGuess.map(num => Number(num));
+}
+
+function getPatternLength() {
+    patternLength = prompt('Define the pattern length (cannot be greater than 6), please: ');
+    patternLength = Number(patternLength);
+
+    validateInput(patternLength, getPatternLength);
+}
+
+function getMaxGuesses() {
+    maxGuesses = prompt('Define a max number of guesses (only numbers), please: ');
+    maxGuesses = Number(maxGuesses);
+
+    validateInput(maxGuesses, getMaxGuesses);
 }
 
 function handleGuesses() {
     while (foundPattern || maxGuesses > guessesCounter) {
-        let patternGuess = prompt('Write your pattern guess, please: ');
+
+        while (patternGuess.length != patternLength) {
+            getPatternGuess();
+        }
 
        foundPattern = valdiatePatternGuess(patternGuess);
+
        guessesCounter++;
 
-       if (foundPatter) {
+       console.log(`Output: ${correctGuesses.length} correct, ${correctPositions} correct position.`)
 
+       correctGuesses = [];
+       correctPositions = 0;
+       patternGuess = [];
+
+       if (foundPattern) {
+        return console.log(`You broke the code in ${guessesCounter} guesses!`);
        }
     }
     
-    
+    return console.log(`You were unable to break the code in ${guessesCounter} guesses. 
+        Code pattern is: ${patternArr.join('')}.`)
 }
 
-function main() {
-    prompt('Hello code maker, welcome to Master Mind game');
+(() => {
+    console.log('Hello code maker, welcome to Master Mind game');
 
-    maxGuesses = prompt('Define a max number of guesses, please: ');
-    maxGuesses = Number(maxGuesses);
+    getMaxGuesses();
 
-    patternLength = prompt('Define the pattern length, please: ');
-    patternLength = Number(patternLength);
+    getPatternLength();
 
     generatePattern();
 
     handleGuesses();
-}
-
-main();
+})();
 
 
 
